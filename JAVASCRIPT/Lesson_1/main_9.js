7c78fc002bfc06670822665112adfbc80a50d0ec
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-07-08 14:40:33 +0700

    JS Basic Ver01.1

--- JAVASCRIPT/Lesson_1/main_9.js
+++ JAVASCRIPT/Lesson_1/main_9.js
@@ -32,3 +32,25 @@ else if(date === 4) {
 else{
     console.log('Khong biet');
 }
+
+//Bai Tap
+/**
+ * Khi a chia het cho 3 thi return ve 1
+ * Khi a chia het cho 5 thi return ve 2
+ * Khi a chia het cho 15 thi return ve 3
+ */
+
+function run(a) {
+    if(a%15 === 0){
+        return 3;
+    }else if(a%3 === 0){
+        return 1;
+    }else if(a%5 === 0){
+       return 2;
+    }
+}
+
+
+console.log(run(3)) // 1
+console.log(run(5)) // 2
+console.log(run(15)) // 3
